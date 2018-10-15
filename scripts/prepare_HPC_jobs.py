43bc0857dabe90979eaefc0f86374456ea6d7b35
nikhil153 <nikhil153@gmail.com>
2018-10-15 15:26:03 -0400

    added function to submit jobs

--- scripts/prepare_HPC_jobs.py
+++ scripts/prepare_HPC_jobs.py
@@ -3,6 +3,7 @@ import argparse
 import sys
 import os
 from shutil import copyfile
+import time
 
 def create_subject_lists(f,subject_list_dir):
     master_df = pd.read_csv(f,sep=',',header=None)
@@ -29,15 +30,18 @@ def create_subject_lists(f,subject_list_dir):
     return sub_list
 
 def create_pipeline_scripts(subx_list_dir,sub_list,model_dir,model_name,beast_dir):
+    # This is tied to minc-toolkit version that's baked into the container
+    MINC_ENV = '/opt/minc/1.9.16/minc-toolkit-config.sh' 
     subject_pipeline_list = []
     subject_list_dir_basename = os.path.basename(subject_list_dir)
     for subx in sub_list:
         subx_script = os.path.join(subject_list_dir, subx, 'run_preproc.sh')
         subject_pipeline_list.append(subx_script)
-        subx_list = 'data/{}/{}/subject.list'.format(subject_list_dir_basename,subx)
+        subx_list_file = 'data/{}/{}/subject.list'.format(subject_list_dir_basename,subx)
         subx_out_dir = 'data/{}/{}/proc_output'.format(subject_list_dir_basename,subx)
-        env_cmd ='#!/bin/bash\nsource /opt/minc/1.9.16/minc-toolkit-config.sh\n'
-	pipeline_cmd = 'python -m scoop nist_mni_pipelines/iplLongitudinalPipeline.py -l {} -o {} -L -D --model-dir={} --model-name={} --beast-dir={}\n'.format(subx_list,subx_out_dir,model_dir,model_name,beast_dir)
+        env_cmd ='#!/bin/bash\nsource {}\n'.format(MINC_ENV)
+	    pipeline_cmd = 'python -m scoop nist_mni_pipelines/iplLongitudinalPipeline.py -l {} -o {} -L -D \
+        --model-dir={} --model-name={} --beast-dir={}\n'.format(subx_list_file,subx_out_dir,model_dir,model_name,beast_dir)
         with open(subx_script, "w") as myfile:
 	    myfile.write(env_cmd)
             myfile.write(pipeline_cmd)
@@ -52,21 +56,51 @@ def create_Qjob_scripts(q_script_header,subject_list_dir,sub_list):
         subx_script = os.path.join(subject_list_dir, subx, q_script_header)
         subject_Qjob_list.append(subx_script)
         copyfile(q_script_header, subx_script)
-        subx_cmd = 'singularity exec --pwd /home/nistmni -B /data/ipl/scratch03/nikhil/containers/test_data:/home/nistmni/data /data/ipl/scratch03/nikhil/containers/minc-tools-docker-v2.simg data/{}/{}/run_preproc.sh\n'.format(subject_list_dir_basename, subx) 
+        subx_cmd = 'singularity exec --pwd /home/nistmni -B /data/ipl/scratch03/nikhil/containers/test_data:/home/nistmni/data \
+        /data/ipl/scratch03/nikhil/containers/minc-tools-docker-v2.simg data/{}/{}/run_preproc.sh\n'.format(subject_list_dir_basename, subx) 
         with open(subx_script, "a") as myfile:
             myfile.write(subx_cmd)
 	os.chmod(subx_script, 0o755)
     print('created {} job scripts at {}'.format(len(sub_list),subject_list_dir))
     return subject_Qjob_list
 
+def submit_HCP_jobs(job_list):
+    """ submits HPC jobs on BIC cluster
+    """
+    print('Submitting jobs to the queue...')
+    msg = ''
+
+    try:    
+        for j in job_list:
+            qsub_cmd = 'qsub -j y -cwd -V -l h_vmem=10G -o out.log {}/qsub_script_header'.format(j)
+            q_status = subprocess.check_output(qsub_cmd, shell=True)
+            time.sleep(1) 
+
+        msg = 'success'
+    except:
+        msg = 'job submission failed'
+## 
+
+
 # argparse
 parser = argparse.ArgumentParser(description = 'Code for creating subject-specifc MR scan (timepoints) jobs for HPC')
-parser.add_argument('--master_list', required=True, help='List of all subjects')
+parser.add_argument('--master_list', required=True, help='List of all subjects. \
+Note that the path needs to be either relative from the mounted directory or absolute within the container.')
+parser.add_argument('--model_dir', required=True, help='Directory for anatomical models, e.g. icbm152. \
+Note that the path needs to be either relative from the mounted directory or absolute within the container.')
+parser.add_argument('--model_name', required=True, help='Name of the model, e.g. mni_icbm152_t1_tal_nlin_sym_09c')
+parser.add_argument('--beast_dir', required=True, help='Directory for BEAST templates. \
+Note that the path needs to be either relative from the mounted directory or absolute within the container.')
+parser.add_argument('--submit_jobs', required=True, help='If true, submit jobs to the HPC. If false, only creates the job list.')
 
 args = parser.parse_args()
 
 # req params    
 master_list_file = args.master_list
+model_dir = args.model_dir #'/opt/minc/share/icbm152_model_09c'
+model_name = args.model_name #'mni_icbm152_t1_tal_nlin_sym_09c'
+beast_dir = args.beast_dir #'/opt/minc/share/beast-library-1.1'
+submit_jobs = args.submit_jobs
 
 # create subject specific lists from a master list
 working_dir = os.path.dirname(master_list_file)
@@ -76,16 +110,12 @@ if not os.path.exists(subject_list_dir):
 sub_list = create_subject_lists(master_list_file,subject_list_dir)
 
 # creare subject specific pipleline script
-model_dir = '/opt/minc/share/icbm152_model_09c'
-model_name = 'mni_icbm152_t1_tal_nlin_sym_09c'
-beast_dir = '/opt/minc/share/beast-library-1.1'
 subject_pipeline_list = create_pipeline_scripts(subject_list_dir,sub_list,model_dir,model_name,beast_dir)
 
 # create subject specific job submission scripts
 subject_Qjob_list = create_Qjob_scripts('qsub_script_header', subject_list_dir, sub_list)
+print(subject_Qjob_list)
 
-#TODO
-# Use Dockerfile entrypoint to source env:
-# source /opt/minc/1.9.16/minc-toolkit-config.sh
-# Mount single input data dir + template dir (read-only)
-# Use customize subject_file names and output dirs for python pipeline command:
+if submit_jobs:
+    q = submit_HCP_jobs(subject_Qjob_list)
+    print(q)
